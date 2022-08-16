e0c9b583307836514d3bc4ef18c3f7ff0cf13a04
nemh <nemh998@gmail.com>
2022-08-16 23:19:53 +0300

    delete and post

--- handelar/postSectorsDataHendler.js
+++ handelar/postSectorsDataHendler.js
@@ -4,8 +4,7 @@
 const {mainsectorModel, admainModel} = require('../Models/Schemh')
 
 
-async function postSectorsDataHendler(req, res)
-{
+async function postSectorsDataHendler(req, res) {
     // need to parse
     // maddleware to decode any request body to json
     console.log("data", req.body)
@@ -14,55 +13,31 @@ async function postSectorsDataHendler(req, res)
     let {subSectorname,
         subSectorLogo,
         subDesignColor,
-        mainSectorid } = req.body
-// 
+        mainSectorid} = req.body
+    // 
 
-console.log(subSectorname,subSectorLogo,subDesignColor,mainSectorid)
-mainsectorModel.findById(mainSectorid,(err,data)=>{
-    if(err){
-        res.send(err)
-    }else{
-        console.log(data)
+    console.log(subSectorname, subSectorLogo, subDesignColor, mainSectorid)
+    mainsectorModel.findById(mainSectorid, (err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            console.log(data)
 
-data.sectors.push({
-    subSectorname:subSectorname,
-    subSectorLogo:subSectorLogo,
-    subDesignColor:subDesignColor,
-    mainSectorid:mainSectorid
+            data.sectors.push({
+                subSectorname: subSectorname,
+                subSectorLogo: subSectorLogo,
+                subDesignColor: subDesignColor,
+                mainSectorid: mainSectorid
 
-})
+            })
 
-data.save()
-res.send(data.sectors)
-
-
-
-}
-})
+            data.save()
+            res.send(data.sectors)
 
 
+
+        }
+    })
 
 }
-
-
 module.exports = postSectorsDataHendler;
-// if (Sectors)
-//     console.log(SectorsData, 'befor save Data')
-
-// SectorsData[0].Sectors.push({
-//     startupName: startupName,
-//     LogoImage: LogoImage,
-//     city: city,
-//     founderName: founderName,
-//     numberOfEmployees: numberOfEmployees,
-//     yearOfEstablishment: yearOfEstablishment,
-//     websiteURL: websiteURL,
-//     emailAddress: emailAddres
-
-
-// })
-// SectorsData[0].save();
-// res.send(SectorsData[0].Sectors)
-// console.log(SectorsData, 'befor save Data')
-// }
-// })
